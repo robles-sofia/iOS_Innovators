7b6e5f52b1a14dc31a2995ebb24706fd96fd0abb
M857pixel <Xshelmg@gmail.com>
2026-02-26 12:29:10 -0600

    Momentum App
    This is the empty Xcode app so that I can create my frontend branch and edit that.

---
+++ Momentum/Momentum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Momentum
+//
+//  Created by Michael Shelton on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Momentum/Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Michael Shelton on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
