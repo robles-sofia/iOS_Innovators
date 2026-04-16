4b631dbb66021ae638e2bd6a94f73edd9a6e8fb9
M857pixel <Xshelmg@gmail.com>
2026-04-16 13:01:06 -0500

    un merge

--- Momentum/Momentum/Models.swift
+++ Momentum/Momentum/Models.swift
@@ -3,53 +3,19 @@
 //  Momentum
 //
 //  Created by Michael Shelton on 3/2/26.
-//  Updated data model and persistence support by Brandon Baker on April 13
 //
 
 import Foundation
 
-// model used to represent a single habit in the app
-// Identifiable allows it to work in lists
-// Hashable allows comparison
-// Codable allows saving/loading from UserDefaults
-struct Habit: Identifiable, Hashable, Codable {
-    
-    // unique id for each habit
-    let id: UUID
-    
-    // name of the habit
+struct Habit: Identifiable, Hashable {
+    let id = UUID()
     var title: String
-    
-    // determines if habit shows up in daily check-in
-    var isSelectedForCheckIn: Bool
-    
-    // tracks if habit is completed for the current day
-    var isDoneToday: Bool
-
-    // custom initializer so defaults can be set when creating habits
-    init(
-        id: UUID = UUID(),
-        title: String,
-        isSelectedForCheckIn: Bool = true,
-        isDoneToday: Bool = false
-    ) {
-        self.id = id
-        self.title = title
-        self.isSelectedForCheckIn = isSelectedForCheckIn
-        self.isDoneToday = isDoneToday
-    }
+    var isSelectedForCheckIn: Bool = true
+    var isDoneToday: Bool = false
 }
 
-// model used for chart data in StatsView
-// represents how many habits were completed on a specific day
 struct DayProgress: Identifiable {
-    
-    // unique id for chart rendering
     let id = UUID()
-    
-    // label for the day (Mon, Tue, etc.)
     let dayLabel: String
-    
-    // number of completed habits for that day
     let completedCount: Int
 }
